acbb16688edde3756bf65a4c866a97f7308c4818
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-03-16 16:00:58 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-importimport { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
