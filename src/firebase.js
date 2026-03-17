821dec0eac98594f50d65d3c98d5cad02df1cbac
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-03-17 14:39:46 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXM-nEcv5HAwXeBVKQ4eSZK8DIzGpRXHE",
-  authDomain: "drik-streaming.firebaseapp.com",
-  projectId: "drik-streaming",
-  storageBucket: "drik-streaming.firebasestorage.app",
-  messagingSenderId: "366702155857",
-  appId: "1:366702155857:web:7a0a922ce0f63c78f50070",
-  measurementId: "G-NVTTBB1PLF"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_PROJETO.firebasestorage.app",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,6 +17,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-  prompt: "select_account",
-});
+export const db = getFirestore(app);
+export const storage = getStorage(app);
