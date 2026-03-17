915de9fc33b87cdc7f582664183a395463e2665c
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-03-17 18:28:18 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID"
+  apiKey: "AIzaSyCXM-nEcv5HAwXeBVKQ4eSZK8DIzGpRXHE",
+  authDomain: "drik-streaming.firebaseapp.com",
+  projectId: "drik-streaming",
+  storageBucket: "drik-streaming.firebasestorage.app",
+  messagingSenderId: "366702155857",
+  appId: "1:366702155857:web:7a0a922ce0f63c78f50070",
+  measurementId: "G-NVTTBB1PLF"
 };
 
 const app = initializeApp(firebaseConfig);
