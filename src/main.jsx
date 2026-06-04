908c6e56d8feea93c9dae1adb2e589b7b4fedbb4
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-06-04 17:43:13 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Aqui ele puxa o treino completo que criamos
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
