ed4bca4106f30e0bb23091699c7672512df979b7
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-06-04 21:21:53 -0300

    Update HeadPose.jsx

--- src/src/components/HeadPose.jsx
+++ src/src/components/HeadPose.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-
 export default function HeadPose() {
   const canvasRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
